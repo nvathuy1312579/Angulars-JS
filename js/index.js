776a39d559816f7707241dd376ae558c4ec6cf6e
Anh Thùy <Anhthuy2908@gmail.com>
2016-05-03 07:39:11 +0700

    MyProfile_ver3

--- js/index.js
+++ js/index.js
@@ -8,17 +8,27 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
             {
       link : '',
       title: 'Full Name: Nguyen Vu Anh Thuy',
-      icon: ''
+      icon: 'description'
     },
     {
       link : '',
       title: 'D.O.B: 23/09/1995',
-      icon: ''
+      icon: 'event'
+    },
+        {
+      link : '',
+      title: 'National: VietNamese',
+      icon: 'language'
+    },
+        {
+      link : '',
+      title: 'Student at Information Technology in University of Sciences',
+      icon: 'account_circle'
     },
     {
       link : '',
       title: 'P.O.B: Kien Giang',
-      icon: ''
+      icon: 'home'
     }
 
   ];
@@ -26,46 +36,23 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
         {
             link : 'anhthuy2908@gmal.com',
             title: 'Email: anhthuy2908@gmail.com',
-            icon: ''
+            icon: 'mail'
         },
         {
             link : '',
             title: 'Phone Number: 01692189779',
-            icon: ''
+            icon: 'phone'
         }
     ];
   
-  $scope.activity = [
+  $scope.SUMMARY = [
       {
         what: 'Brunch this weekend?',
         who: 'Ali Conners',
         when: '3:08PM',
         notes: " I'll be in your neighborhood doing errands"
       },
-      {
-        what: 'Summer BBQ',
-        who: 'to Alex, Scott, Jennifer',
-        when: '3:08PM',
-        notes: "Wish I could come out but I'm out of town this weekend"
-      },
-      {
-        what: 'Oui Oui',
-        who: 'Sandra Adams',
-        when: '3:08PM',
-        notes: "Do you have Paris recommendations? Have you ever been?"
-      },
-      {
-        what: 'Birthday Gift',
-        who: 'Trevor Hansen',
-        when: '3:08PM',
-        notes: "Have any ideas of what we should get Heidi for her birthday?"
-      },
-      {
-        what: 'Recipe to try',
-        who: 'Brian Holt',
-        when: '3:08PM',
-        notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
-      },
+            
     ];
   $scope.alert = '';
   $scope.showListBottomSheet = function($event) {
