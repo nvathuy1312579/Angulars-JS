bd4046fc853e9722209edad65fe0142beb33d9da
Anh Thùy <Anhthuy2908@gmail.com>
2016-05-06 19:55:33 +0700

    profile_ver5

--- js/index.js
+++ js/index.js
@@ -1,59 +1,60 @@
 var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons']);
 
+/*var myapp = angular.module("AccountApp", []);*/
+
+app.service("profile", function ($http, $q)
+{
+	var deferred = $q.defer();
+	$http.get('https://api.myjson.com/bins/uwjk').then(function (data)
+	{
+		deferred.resolve(data);
+	});
+
+	this.getMenu   = function ()
+	{
+		return deferred.promise;
+	}
+})
+
+app.directive('repeatDone', function() {
+    return function(scope, element, attrs) {
+        if (scope.$last) { // all are rendered
+            scope.$eval(attrs.repeatDone);
+        }
+    }
+});
+
+app.controller("accountCtrl", function ($scope, profile)
+{
+    window.sc1=$scope;
+	var promise = profile.getMenu();
+  $scope.data = null;
+	promise.then(function (data)
+	{
+		$scope.data = data.data;
+    
+		console.log($scope.menu);
+	});
+  $scope.current = 0;
+  $scope.checkItem = function (params) {
+    $scope.current = parseFloat(params);
+    $scope.exp = true;
+  }
+   $scope.update_exp = function () {
+    if($scope.exp_company != undefined)
+      $scope.data.experience[$scope.current].company = $scope.exp_company;
+  }
+});
+
 app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog', function($scope, $mdBottomSheet, $mdSidenav, $mdDialog){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
- 	$scope.menu = [
-            {
-      link : '',
-      title: 'Full Name: Nguyen Vu Anh Thuy',
-      icon: 'description'
-    },
-    {
-      link : '',
-      title: 'D.O.B: 23/09/1995',
-      icon: 'event'
-    },
-        {
-      link : '',
-      title: 'National: VietNamese',
-      icon: 'language'
-    },
-        {
-      link : '',
-      title: 'Student at Information Technology in University of Sciences',
-      icon: 'account_circle'
-    },
-    {
-      link : '',
-      title: 'P.O.B: Kien Giang',
-      icon: 'home'
-    }
-
-  ];
-    $scope.contacts = [
-        {
-            link : 'anhthuy2908@gmal.com',
-            title: 'Email: anhthuy2908@gmail.com',
-            icon: 'mail'
-        },
-        {
-            link : '',
-            title: 'Phone Number: 01692189779',
-            icon: 'phone'
-        }
-    ];
   
-  $scope.SUMMARY = [
-      {
-        what: 'Brunch this weekend?',
-        who: 'Ali Conners',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
-      },
-            
-    ];
+  $scope.initModals = function() {
+    $('.modal-trigger').leanModal(); // Initialize the modals
+}
+    
   $scope.alert = '';
   $scope.showListBottomSheet = function($event) {
     $scope.alert = '';
@@ -78,6 +79,14 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
       $scope.alert = 'You cancelled the dialog.';
     });
   };
+  
+  $scope.current = 0;
+  $scope.exp=[0,0,0];
+  $scope.exp=[false, false, false];
+  $scope.checkItem = function (params) {
+    $scope.exp[params]=true;
+  }
+ 
 }]);
 
 app.controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
@@ -94,6 +103,62 @@ app.controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
   };
 });
 
+app.controller("LocationFormCtrl", function ($scope) {
+    $scope.location = {
+        region: "Wellington Region",
+        city: "Wellington",
+        suburb: "Mt. Cook"
+    };
+});
+app.directive("clickToEdit", function () {
+    var editorTemplate = '' +
+        '<div class="click-to-edit">' +
+            '<div ng-hide="view.editorEnabled">' +
+                '<p>{{value}}</p>' + '<md-button ng-click="enableEditor()" aria-label="Add"><ng-md-icon icon="edit"></ng-md-icon></md-button>'+
+            '</div>' +
+            '<div ng-show="view.editorEnabled">' +
+                '<input type="text" ng-model="view.editableValue">' +
+                '<md-button ng-click="save()" aria-label="save"><p>Save </p></md-button>'+
+                ' or ' +
+                '<md-button ng-click="disableEditor()" aria-label="Cancel"><p>Cancel </p></md-button>'+
+            '</div>' +
+        '</div>';
+
+    return {
+        restrict: "A",
+        replace: true,
+        template: editorTemplate,
+        scope: {
+            value: "=clickToEdit",
+        },
+        link: function (scope, element, attrs) {
+            scope.view = {
+                editableValue: scope.value,
+                editorEnabled: false
+            };
+
+            scope.enableEditor = function () {
+                scope.view.editorEnabled = true;
+                scope.view.editableValue = scope.value;
+                setTimeout(function () {
+                    element.find('input')[0].focus();
+                    //element.find('input').focus().select(); // w/ jQuery
+                });
+            };
+
+            scope.disableEditor = function () {
+                scope.view.editorEnabled = false;
+            };
+
+            scope.save = function () {
+                scope.value = scope.view.editableValue;
+                scope.disableEditor();
+            };
+
+        }
+    };
+});
+
 function DialogController($scope, $mdDialog) {
   $scope.hide = function() {
     $mdDialog.hide();
@@ -105,13 +170,6 @@ function DialogController($scope, $mdDialog) {
     $mdDialog.hide(answer);
   };
 };
-
-app.directive('userAvatar', function() {
-  return {
-    replace: true,
-    template: '<svg class="user-avatar" viewBox="0 0 128 128" height="64" width="64" pointer-events="none" display="block" > <path fill="#FF8A80" d="M0 0h128v128H0z"/> <path fill="#FFE0B2" d="M36.3 94.8c6.4 7.3 16.2 12.1 27.3 12.4 10.7-.3 20.3-4.7 26.7-11.6l.2.1c-17-13.3-12.9-23.4-8.5-28.6 1.3-1.2 2.8-2.5 4.4-3.9l13.1-11c1.5-1.2 2.6-3 2.9-5.1.6-4.4-2.5-8.4-6.9-9.1-1.5-.2-3 0-4.3.6-.3-1.3-.4-2.7-1.6-3.5-1.4-.9-2.8-1.7-4.2-2.5-7.1-3.9-14.9-6.6-23-7.9-5.4-.9-11-1.2-16.1.7-3.3 1.2-6.1 3.2-8.7 5.6-1.3 1.2-2.5 2.4-3.7 3.7l-1.8 1.9c-.3.3-.5.6-.8.8-.1.1-.2 0-.4.2.1.2.1.5.1.6-1-.3-2.1-.4-3.2-.2-4.4.6-7.5 4.7-6.9 9.1.3 2.1 1.3 3.8 2.8 5.1l11 9.3c1.8 1.5 3.3 3.8 4.6 5.7 1.5 2.3 2.8 4.9 3.5 7.6 1.7 6.8-.8 13.4-5.4 18.4-.5.6-1.1 1-1.4 1.7-.2.6-.4 1.3-.6 2-.4 1.5-.5 3.1-.3 4.6.4 3.1 1.8 6.1 4.1 8.2 3.3 3 8 4 12.4 4.5 5.2.6 10.5.7 15.7.2 4.5-.4 9.1-1.2 13-3.4 5.6-3.1 9.6-8.9 10.5-15.2M76.4 46c.9 0 1.6.7 1.6 1.6 0 .9-.7 1.6-1.6 1.6-.9 0-1.6-.7-1.6-1.6-.1-.9.7-1.6 1.6-1.6zm-25.7 0c.9 0 1.6.7 1.6 1.6 0 .9-.7 1.6-1.6 1.6-.9 0-1.6-.7-1.6-1.6-.1-.9.7-1.6 1.6-1.6z"/> <path fill="#E0F7FA" d="M105.3 106.1c-.9-1.3-1.3-1.9-1.3-1.9l-.2-.3c-.6-.9-1.2-1.7-1.9-2.4-3.2-3.5-7.3-5.4-11.4-5.7 0 0 .1 0 .1.1l-.2-.1c-6.4 6.9-16 11.3-26.7 11.6-11.2-.3-21.1-5.1-27.5-12.6-.1.2-.2.4-.2.5-3.1.9-6 2.7-8.4 5.4l-.2.2s-.5.6-1.5 1.7c-.9 1.1-2.2 2.6-3.7 4.5-3.1 3.9-7.2 9.5-11.7 16.6-.9 1.4-1.7 2.8-2.6 4.3h109.6c-3.4-7.1-6.5-12.8-8.9-16.9-1.5-2.2-2.6-3.8-3.3-5z"/> <circle fill="#444" cx="76.3" cy="47.5" r="2"/> <circle fill="#444" cx="50.7" cy="47.6" r="2"/> <path fill="#444" d="M48.1 27.4c4.5 5.9 15.5 12.1 42.4 8.4-2.2-6.9-6.8-12.6-12.6-16.4C95.1 20.9 92 10 92 10c-1.4 5.5-11.1 4.4-11.1 4.4H62.1c-1.7-.1-3.4 0-5.2.3-12.8 1.8-22.6 11.1-25.7 22.9 10.6-1.9 15.3-7.6 16.9-10.2z"/> </svg>'
-  };
-});
 
 app.config(function($mdThemingProvider) {
   var customBlueMap = 		$mdThemingProvider.extendPalette('light-blue', {
@@ -128,4 +186,9 @@ app.config(function($mdThemingProvider) {
     .accentPalette('pink');
   $mdThemingProvider.theme('input', 'default')
         .primaryPalette('grey')
+});
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
 });
